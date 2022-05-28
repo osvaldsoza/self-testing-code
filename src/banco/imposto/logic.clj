df135252093ecf06711da437a0eb6147525b5167
osvaldsoza <osvald.soza@gmail.com>
2022-05-28 13:04:29 -0300

    atualizacao

--- src/banco/imposto/logic.clj
+++ src/banco/imposto/logic.clj
@@ -1,8 +1,14 @@
 (ns banco.imposto.logic)
 
+;;funcao privada
+(defn- esta-na-faixa-de-isencao?
+  [valor]
+  (let [teto-salario 1000]
+    (< valor teto-salario)))
 
 (defn imposto-retido-fonte
   [valor]
-  (if (< valor 1000)
-    0
-    (* valor 0.1)))
+  (let [taxa 0.1]
+    (if (esta-na-faixa-de-isencao? valor)
+      0
+      (* valor taxa))))
